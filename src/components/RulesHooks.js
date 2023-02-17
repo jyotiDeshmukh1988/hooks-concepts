15eee7d972bd4766e2aa493975bf3c80324ba233
Jyoti Deshmukh <test8600896492@gmail.com>
2023-02-17 15:43:03 +0530

    rules of creating the component and hooks in the component

--- src/components/RulesHooks.js
+++ src/components/RulesHooks.js
@@ -1,9 +1,8 @@
-// rules for the hooks
+// rules for the hooks usage and component creation
 // 1 - Aiways write it inside the component or function
 // 2 - Component name must be PascalCase(first letter should be uppercase)
 // 3 - We can directly import or we can directly write use it Reach.hookname
 // 4 - Do not call hooks inside loops, conditions, or nested functions.
-
 
 import React from 'react'
 
