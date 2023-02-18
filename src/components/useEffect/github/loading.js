d16c5f7058c4502bb3a7b30f059296a187d69c1e
Jyoti Deshmukh <test8600896492@gmail.com>
2023-02-19 01:29:24 +0530

    Adding Error Handling, Loading Functionality in API fetch using useEffect

---
+++ src/components/useEffect/github/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='loadingDiv'><h2 style={{color:'black'}}>Loading....</h2></div>
+  )
+}
+
+export default Loading
