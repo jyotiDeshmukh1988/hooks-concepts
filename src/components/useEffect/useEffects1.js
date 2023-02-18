478810e7df64aa712efef351900a331a0069751a
Jyoti Deshmukh <test8600896492@gmail.com>
2023-02-18 23:25:29 +0530

    added the difference useEffectwith dependicies

--- src/components/useEffect/useEffects1.js
+++ src/components/useEffect/useEffects1.js
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 const UseEffects1 = () => {
     const[count,setCount] = useState(0)
+    const [name,setName] = useState()
+
     useEffect(()=>{
-        console.log('Hello inside UseEffects')
-        document.title = ` chats (${count})`
-    })
+        console.log('i am great');
+        (count > 0) ? document.title = ` chats (${count})` :document.title = `chats`
+    },[count])
+
+    useEffect(()=>{
+        console.log('i am fine')
+    },[])
+
     console.log('Hello outside UseEffects')
     const increment = () => {
         setCount(count+1)
@@ -13,10 +20,15 @@ const UseEffects1 = () => {
     const decrement = () => {
         count > 0 && setCount(count-1)
     }
+    const changeName = () => {
+        setName("jyoti")
+    }
   return (
     <div>
         <h1>UseEffects1</h1>
         <h2>{count}</h2>
+        <h3>{name}</h3>
+        <button onClick={changeName}>Change Name</button><br/><br/>
         <button onClick={increment}>Increment</button> <button onClick={decrement}>Decrement</button>
     </div>
   )
